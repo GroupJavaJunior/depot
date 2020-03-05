ac5ddeb74f802aa161526019b4e66b89bbb36e9a
m.shestakov <shestakov7577412@gmail.com>
2020-03-06 02:23:01 +0300

    Add css for index page, change html index page, update seeds for Products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +7,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.delete_all
+
+products = [
+  { title: 'Product title', description: %(Some description),
+    image_url: '1.jpeg', price: 49.95 },
+  { title: 'Product title 2', description: %(Some description for product 2),
+    image_url: '2.png', price: 39.95 },
+  { title: 'Product title 3', description: %(Some description for product 3),
+    image_url: '3.jpeg', price: 29.95 },
+  { title: 'Product title 4', description: %(Some description for product 4),
+    image_url: '4.jpg', price: 19.95 }
+]
+
+products.each { |product| Product.create!(product) }
