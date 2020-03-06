21cfcdf708f2709e2d7b1321c3ff1868064873a2
m.shestakov <shestakov7577412@gmail.com>
2020-03-06 20:14:51 +0300

    Add tests for product model

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,75 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :products
+
+  test 'product model' do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test 'product price must be positive' do
+    product = Product.new(title: 'My book title',
+                          description: 'yyy',
+                          image_url: 'zzz.jpg')
+
+    product.price = -1
+    assert product.invalid?
+    assert_equal ['must be greater than or equal to 0.01'],
+                 product.errors[:price]
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal ['must be greater than or equal to 0.01'],
+                 product.errors[:price]
+
+    product.price = 1
+    assert product.valid?
+  end
+
+  def new_product(image_url)
+    Product.new(title: 'My book title',
+                description: 'yyy',
+                price: 1,
+                image_url: image_url)
+  end
+
+  test 'image_url' do
+    ok = %w[fred.gif fred.jpg fred.jpeg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif]
+    bad = %w[fred.doc fred.gif/more fred.gif.more]
+
+    ok.each do |img_name|
+      assert new_product(img_name).valid?, "#{img_name} should be valid"
+    end
+
+    bad.each do |img_name|
+      assert new_product(img_name).invalid?, "#{img_name} should be invalid"
+    end
+  end
+
+  test 'product is not valid without a unique title' do
+    product = Product.new(title: products(:ruby).title,
+                          description: 'yyy',
+                          price: 1,
+                          image_url: 'fred.gif')
+
+    assert product.invalid?
+
+    assert_equal ['has already been taken'], product.errors[:title]
+  end
+
+  test 'length' do
+    product = products(:ruby)
+
+    assert product.valid?
+
+    product.title = '1'
+    assert product.invalid?
+  end
 end
