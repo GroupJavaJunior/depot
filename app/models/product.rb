4ae7530ceaf198b63d562456380d9abefa0f047c
m.shestakov <shestakov7577412@gmail.com>
2020-03-07 11:04:36 +0300

    Displaying store of products

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,8 @@ class Product < ActiveRecord::Base
   validates :title, uniqueness: true, length: { minimum: 2 }
   validates :image_url, allow_blank: true, format: { with: /\.(gif|jpg|png|jpeg)\Z/i,
                                                      message: 'must be a URL for GIF, JPG or PNG image.' }
+
+  def self.latest
+    Product.order(:updated_at).last
+  end
 end
